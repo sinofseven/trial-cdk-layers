e621f3c3e3b20e55ee01b6b9532d1b4f4f60ae80
sinofseven <em.s.00001@gmail.com>
2024-04-03 16:12:01 +0900

    Initial commit

---
+++ test/trial_cdk_layers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrialCdkLayers from '../lib/trial_cdk_layers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trial_cdk_layers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrialCdkLayers.TrialCdkLayersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
